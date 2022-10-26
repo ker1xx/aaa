6d3e0cd47984b0cf21f892e82cd42ca43d9baef8
ker1xx <112649840+ker1xx@users.noreply.github.com>
2022-10-26 21:48:41 +0300

    Update Program.cs

--- prac4/Program.cs
+++ prac4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.Data;
 using System.IO.IsolatedStorage;
 using System.Linq.Expressions;
@@ -13,23 +13,23 @@ namespace prac4
         static void Main(string[] args)
         {
             Z4m3tk1 zametka1 = new Z4m3tk1();
-            zametka1.name = "  blazer)";
+            zametka1.name = "blazer)";
             zametka1.data = new DateTime(2007, 11, 21);
             zametka1.description = "  pit' blazer posle 4 yroka";
             Z4m3tk1 zametka2 = new Z4m3tk1();
-            zametka2.name = "  стать эмо)";
+            zametka2.name = "стать эмо)";
             zametka2.data = new DateTime(2007, 7, 20);
             zametka2.description = "  послушать токиохотель и резать вены";
             Z4m3tk1 zametka3 = new Z4m3tk1();
-            zametka3.name = "  поехать на казантип";
+            zametka3.name = "поехать на казантип";
             zametka3.data = new DateTime(2007, 7, 25);
             zametka3.description = "  словить трип в рейве";
             Z4m3tk1 zametka4 = new Z4m3tk1();
-            zametka4.name = "  школа...";
+            zametka4.name = "школа...";
             zametka4.data = new DateTime(2007, 7, 23);
             zametka4.description = "  сходи на уроки и выучить уроки";
             Z4m3tk1 zametka5 = new Z4m3tk1();
-            zametka5.name = "  получить по башке от скинов";
+            zametka5.name = "получить по башке от скинов";
             zametka5.data = new DateTime(2007, 7, 23);
             zametka5.description = "  получить по башке а потом в травмпункт и к маме домой зато в школу не иду)))))";
             List<Z4m3tk1> zametki = new List<Z4m3tk1> { zametka1, zametka2, zametka3, zametka4, zametka5 };
@@ -45,7 +45,7 @@ namespace prac4
                 {
                     pos--;
                     if (pos == 0)
-                        pos = sortedZam.Count;
+                        pos = 1;
                 }
                 if (key.Key == ConsoleKey.DownArrow && sortedZam_count != 0)
                 {
@@ -58,7 +58,7 @@ namespace prac4
                 int op_pos = pos - 1;
                 foreach (var item in sortedZam)
                 {
-                    Console.WriteLine(item.name);
+                    Console.WriteLine("  " + item.name);
                     for (int i = 0; i <= op_pos; i++)
                         if (key.Key == ConsoleKey.Enter)
                         {
@@ -100,23 +100,71 @@ namespace prac4
             Console.WriteLine("Введите название заметки: ");
             new_zametki.name = Console.ReadLine();
             new_zametki.name = new_zametki.name + "  ";
-            Console.WriteLine("Введите дату, на которую будет назначена заметка (вводите дату в формате 'гггг.мм.дд', например 01.01.0001: ");
+            Console.WriteLine("Введите дату, на которую будет назначена заметка (вводите дату в формате 'дд.мм.гггг', например 01.01.0001: ");
             string data = Console.ReadLine();
             string[] num_split = data.Split(".");
-            for (int i = 0; i < num_split.Length; i++)
-                Console.WriteLine(num_split[i]);
             if (data.Length != 10)
             {
                 Console.WriteLine("Вы ввели дату в неверном формате");
                 return null;
             }
+            double data0 = Convert.ToDouble(num_split[0]);
+            double data1 = Convert.ToDouble(num_split[1]);
+            double data2 = Convert.ToDouble(num_split[2]);
+            int visok = 0;
+            if (data2 % 4 == 0)
+                visok = 1;
             if (num_split[0].Length != 2)
             {
                 Console.WriteLine("Вы ввели дату в неверном формате");
                 return null;
             }
+            switch (data1)
+            {
+                case (1):
+                case (3):
+                case (5):
+                case (7):
+                case (8):
+                case (10):
+                case (12):
+                    if ((data0 > 31) || (data0 < 0))
+                    {
+                        Console.WriteLine("Вы ввели дату в неверном формате");
+                        return null;
+                    }
+                    break;
+                case (2):
+                    if (visok == 0)
+                    {
+                        if ((data0 > 28) || (data0 < 0))
+                        {
+                            Console.WriteLine("Вы ввели дату в неверном формате");
+                            return null;
+                        }
+                    }
+                    else
+                         if ((data0 > 29) || (data0 < 0))
+                    {
+                        Console.WriteLine("Вы ввели дату в неверном формате");
+                        return null;
+                    }
+                    break;
+                case (4):
+                case (6):
+                case (9):
+                case (11):
+                    if ((data0 > 30) || (data0 < 0))
+                    {
+                        Console.WriteLine("Вы ввели дату в неверном формате");
+                        return null;
+                    }
+                    break;
+
+            }
             if (num_split[1].Length != 2)
             {
+
                 Console.WriteLine("Вы ввели дату в неверном формате");
                 return null;
             }
@@ -128,9 +176,9 @@ namespace prac4
             new_zametki.data = new DateTime(Convert.ToInt32(num_split[2]), Convert.ToInt32(num_split[1]), Convert.ToInt32(num_split[0]));
             Console.WriteLine("Введите описание заметки: ");
             new_zametki.description = Console.ReadLine();
-            new_zametki.description = new_zametki.description + "  ";
-            return new_zametki;
+            new_zametki.description = new_zametki.description ;
             Console.WriteLine("Вы добавили заметку. Чтобы вернуться в меню заметок, нажмите стрелку влево или вправо");
+            return new_zametki;
 
         }
     }
